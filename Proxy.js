f17e977e3fa67de416fda25451715dd5aede3d46
Xing Wei <xingw@xingw-mbpro.local>
2015-06-12 09:46:02 -0700

    Formated code

--- Proxy.js
+++ Proxy.js
@@ -55,41 +55,7 @@ if ('development' == proxyApp.get('env')) {
 proxyApp.get('/*', function(webRequest, response) {
 	var request = require('request').debug=true;
 	console.log('GET Request:'+webRequest.url);
-	// request({
- //    		uri  : proxiedHost 
-	// }, 
-	// webRequest.on('end', 
-	// function(err, res, body) {
-    		//console.log('REQUEST RESULTS:', err, res.statusCode, body);
-		// response.writeHead(200, { 'Content-Type': 'text/html' });
-		// if(body){
-		// 	response.write('test');
-		// }
-		// response.end();
-	// });
-	// response.send("Hello GET");
-
-	// Simple Version:
-	// var options = {
-	// 	host: proxiedHost,
- 	//  path: '/index.html'
-	// };
-	// var callback = function(res){
-	// 	var body = '';
-	// 	res.on('data', function(data){
-	// 		body += data;
-	// 	});
-
-	// 	res.on('end', function(){
-	// 		console.log(body);
-	// 		response.send(body);
-	// 	});
-
-	// }
-
-	// var req = http.request(options, callback);
-	// req.end();
-
+	
 	var request = require('request');
 	var jar = request.jar();
 	var headers = webRequest.headers;
@@ -135,44 +101,6 @@ proxyApp.get('/*', function(webRequest, response) {
 			body:data 
 		};
 
-		// send the request to the destination and save the response to local file 
-		// request.get(options, callback).pipe(
-		// 	fs.createWriteStream(serviceName+'/Response'+requestCount+'.txt'),{end:false});
-		
-		// Optional ways
-		// request.get('http://google.com').on('response', function(resp){
-		// 	console.log(resp.statusCode); // 200 
-  //   		console.log(JSON.stringify(resp)); 
-  //   		response.send(resp);
-  // 		}).pipe(fs.createWriteStream(serviceName+'/Response'+requestCount+'.txt'),{end:false});
-
-		// function callback(error, cbresponse, body) {
-  //       	console.log('--------------------[ endpoint Response '+currentCount+ ' ]---------------');
-		// 	var cbheaders = cbresponse.headers;
-		// 	console.log('Headers from endpoint:'+JSON.stringify(cbheaders));
-		
-		// 	// headers to return
-		// 	var rtnHeaders = {};
-		// 	for(var key in cbheaders) {
-  //   			var value = cbheaders[key];
-		// 		if(key!='content-length'&&key!='host'){
-		// 			rtnHeaders[key]=value;
-		// 		}
-		// 	}
-		// 	// write header to local file
-		// 	fs.writeFile(serviceName+'/ResponseHeader'+requestCount+'.txt', JSON.stringify(rtnHeaders), function (err) {
-  // 				if (err) throw err;
-		// 	});
-
-		// 	// send back the response
-		// 	console.log('Response Code:'+cbresponse.statusCode); 
-		// 	response.writeHead(cbresponse.statusCode,cbheaders);
-		// 	response.write(body);
-		// 	response.end();
-
-  //       	console.log('--------------------[ /endpoint Response '+currentCount+ ' ]---------------');
-		// };
-
   		request(options, function (error, resp, body) {
     		if (!error && response.statusCode == 200) {
         		console.log(body);
@@ -203,11 +131,11 @@ proxyApp.post('/*', function(webRequest, response) {
 	console.log('POST Request:'+webRequest.url);
 	console.log('POST Headers:'+JSON.stringify(headers));
 
-        webRequest.on('data', function(chunk) {
-                data+=chunk.toString();
-        });
+    webRequest.on('data', function(chunk) {
+        data+=chunk.toString();
+    });
 
-webRequest.on('end', function() {
+	webRequest.on('end', function() {
 		var currentCount = requestCount;
 		requestCount++;
 
@@ -218,13 +146,13 @@ webRequest.on('end', function() {
 		});
 
 		function callback(error, cbresponse, body) {
-        		console.log('--------------------[ endpoint Response '+currentCount+ ' ]---------------');
+        	console.log('--------------------[ endpoint Response '+currentCount+ ' ]---------------');
 			var cbheaders = cbresponse.headers;
 			console.log('Headers from endpoint:'+JSON.stringify(cbheaders));
 		
 			var rtnHeaders = {};
 			for(var key in cbheaders) {
-    				var value = cbheaders[key];
+    			var value = cbheaders[key];
 				//console.log('HEADER:'+key+':'+value);
 				if(key!='content-length'&&key!='host'){
 					rtnHeaders[key]=value;
@@ -235,19 +163,18 @@ webRequest.on('end', function() {
   				if (err) throw err;
 			});
 
-
 			console.log('Response Code:'+cbresponse.statusCode); // + '   Body:'+body);
 			response.writeHead(cbresponse.statusCode,cbheaders);
 			response.write(body);
 			response.end();
 
-        		console.log('--------------------[ /endpoint Response '+currentCount+ ' ]---------------');
+        	console.log('--------------------[ /endpoint Response '+currentCount+ ' ]---------------');
 		};
 
 		//console.log("Posting Data:"+data);
 		var newHeaders = {};
 		for(var key in webRequest.headers) {
-    			var value = webRequest.headers[key];
+    		var value = webRequest.headers[key];
 			//console.log('HEADER:'+key+':'+value);
 			if(key!='content-length'&&key!='host'){
 				newHeaders[key]=value;
@@ -266,7 +193,7 @@ webRequest.on('end', function() {
 		request.post(options, callback).pipe(fs.createWriteStream(serviceName+'/Response'+requestCount+'.txt'),{end:false});
 	});
 
-        console.log('--------------------[ /simulation Request '+currentRequestNum+' ]---------------');
+    console.log('--------------------[ /simulation Request '+currentRequestNum+' ]---------------');
 
 });
 
