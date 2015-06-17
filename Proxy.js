fbbf5a1d2390db1483270e64f0c03dda6af73441
Xing Wei <xingw1@andrew.cmu.edu>
2015-06-17 13:49:15 -0700

    Fixed the problem with font and formated code

--- Proxy.js
+++ Proxy.js
@@ -17,9 +17,21 @@ process.argv.forEach(function (val, index, array) {
 	}
 });
 
+//---------------[ Parse the url to be proxied]---------------//
+var parts = proxiedHost.split(':');
+var hostName = '';
+var portNumber = '';
+if(parts.length == 2){
+	hostName = parts[1].substring(2);
+	portNumber = 80;
+}
+else if(parts.length == 3){
+	hostName = parts[1].substring(2);
+	portNumber = parts[2];
+}
 
-console.log('Proxying for host:'+proxiedHost);
-console.log('Proxying for service:'+serviceName);
+console.log('Proxying for host: '+hostName + ' on '+ portNumber);
+console.log('Proxying for service: '+serviceName);
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
@@ -68,11 +80,6 @@ proxyApp.get('/*', function(webRequest, response) {
 	console.log('GET Request:'+webRequest.url);
 	console.log('GET Headers:'+JSON.stringify(headers));
 
-	// webRequest.on('data', function(chunk) {
-	// 	data+=chunk.toString();
-	// });
-
-	// webRequest.on('end', function() {
 		console.log(data); // data should be nothing here
 		var currentCount = requestCount;
 		requestCount++;
@@ -85,7 +92,7 @@ proxyApp.get('/*', function(webRequest, response) {
 		});
 
 		// NEXT - redirect the request to the destination
-		// create a new header
+		// create a new header - didn't use it in this build
 		var newHeaders = {};
 		for(var key in webRequest.headers) {
     		var value = webRequest.headers[key];
@@ -99,12 +106,13 @@ proxyApp.get('/*', function(webRequest, response) {
     		return str.indexOf(suffix, str.length - suffix.length) !== -1;
 		}
 
-		// If it's image
-		if(!endsWith(webRequest.url, 'png') && !endsWith(webRequest.url, 'jpg')){
+		// To handle text 
+		if(!endsWith(webRequest.url, 'png') && !endsWith(webRequest.url, 'jpg') 
+			&& !endsWith(webRequest.url, 'ttf') && !endsWith(webRequest.url, 'woff')){
 			// prepare redirecting request options
 			var options = {
 				uri:proxiedHost + webRequest.url, 
-				headers: webRequest.headers, 
+				// headers: webRequest.headers, 
 				jar:true, 
 			};
 
@@ -131,12 +139,13 @@ proxyApp.get('/*', function(webRequest, response) {
     			}
 			});
 		}
+		// To handle images or font, which requires to be encoded in binary
 		else{	
-			console.log('**********'+proxiedHost);
 			options = {
-			    host: 'localhost'
-			  , port: 8080
+			    host: hostName
+			  , port: portNumber
 			  , path: webRequest.url
+			  	// uri: proxiedHost + webRequest.url
 			}
 
 			var request = http.get(options, function(res){
@@ -145,7 +154,6 @@ proxyApp.get('/*', function(webRequest, response) {
 
 				res.on('data', function(chunk){
 			    	imagedata += chunk
-			    	// res.pipe(response);
 				})
 
 				res.on('end', function(){
@@ -175,73 +183,62 @@ proxyApp.post('/*', function(webRequest, response) {
 	console.log(queryBody);
 	data = JSON.stringify(queryBody);
 
+	console.log('POST body:'+data);
+	var currentCount = requestCount;
+	requestCount++;
 
-	// webRequest.on('data', function(chunk) {
-	// 	data+=chunk.toString();
-	// 	console.log('POST body:'+data);
-	// });
+	var rqst = {'path':webRequest.path, 'method':'post', 'headers':webRequest.headers, 'body':data};
 
-	// webRequest.on('end', function() {
-		console.log('POST body:'+data);
-		var currentCount = requestCount;
-		requestCount++;
+	fs.writeFile(serviceName+'/Request'+requestCount+'.txt', JSON.stringify(rqst), function(err) {
+		if (err) throw err;
+	});
 
-		var rqst = {'path':webRequest.path, 'method':'post', 'headers':webRequest.headers, 'body':data};
-
-		fs.writeFile(serviceName+'/Request'+requestCount+'.txt', JSON.stringify(rqst), function(err) {
-			if (err) throw err;
-		});
-
-		function callback(error, cbresponse, body) {
-        	console.log('--------------------[ endpoint Response '+currentCount+ ' ]---------------');
-			var cbheaders = cbresponse.headers;
-			console.log('Headers from endpoint:'+JSON.stringify(cbheaders));
+	function callback(error, cbresponse, body) {
+    	console.log('--------------------[ endpoint Response '+currentCount+ ' ]---------------');
+		var cbheaders = cbresponse.headers;
+		console.log('Headers from endpoint:'+JSON.stringify(cbheaders));
 		
-			var rtnHeaders = {};
-			for(var key in cbheaders) {
-    			var value = cbheaders[key];
-				//console.log('HEADER:'+key+':'+value);
-				if(key!='content-length'&&key!='host'){
-					rtnHeaders[key]=value;
-				}
-			}
-
-			fs.writeFile(serviceName+'/ResponseHeader'+requestCount+'.txt', JSON.stringify(rtnHeaders), function (err) {
-  				if (err) throw err;
-			});
-
-			console.log('Response Code:'+cbresponse.statusCode); // + '   Body:'+body);
-			response.writeHead(cbresponse.statusCode,cbheaders);
-			response.write(body);
-			response.end();
-
-        	console.log('--------------------[ /endpoint Response '+currentCount+ ' ]---------------');
-		};
-
-		//console.log("Posting Data:"+data);
-		var newHeaders = {};
-		for(var key in webRequest.headers) {
-    		var value = webRequest.headers[key];
+		var rtnHeaders = {};
+		for(var key in cbheaders) {
+    		var value = cbheaders[key];
 			//console.log('HEADER:'+key+':'+value);
 			if(key!='content-length'&&key!='host'){
-				newHeaders[key]=value;
+				rtnHeaders[key]=value;
 			}
 		}
 
-		console.log('Send Headers:'+JSON.stringify(newHeaders));
+		fs.writeFile(serviceName+'/ResponseHeader'+requestCount+'.txt', JSON.stringify(rtnHeaders), function (err) {
+  			if (err) throw err;
+		});
 
-		var options = {
-			uri:proxiedHost+webRequest.url
-			//, headers: {"content-type":"text/xml; charset=utf-8","soapaction":"urn:vim25/5.5","user-agent":"VMware vim-java 1.0"}
-			// , headers: newHeaders
-			, headers: webRequest.headers
-			, jar:true
-			, body:data
-		};
-		request.post(options, callback).pipe(fs.createWriteStream(serviceName+'/Response'+requestCount+'.txt'),{end:false});
-		// webRequest.end();
-	// });
+		console.log('Response Code:'+cbresponse.statusCode); // + '   Body:'+body);
+		response.writeHead(cbresponse.statusCode,cbheaders);
+		response.write(body);
+		response.end();
 
+        console.log('--------------------[ /endpoint Response '+currentCount+ ' ]---------------');
+	};
+
+	var newHeaders = {};
+	for(var key in webRequest.headers) {
+    	var value = webRequest.headers[key];
+		//console.log('HEADER:'+key+':'+value);
+		if(key!='content-length'&&key!='host'){
+			newHeaders[key]=value;
+		}
+	}
+
+	console.log('Send Headers:'+JSON.stringify(newHeaders));
+
+	var options = {
+		uri:proxiedHost+webRequest.url
+		//, headers: {"content-type":"text/xml; charset=utf-8","soapaction":"urn:vim25/5.5","user-agent":"VMware vim-java 1.0"}
+		// , headers: newHeaders
+		, headers: webRequest.headers
+		, jar:true
+		, body:data
+	};
+	request.post(options, callback).pipe(fs.createWriteStream(serviceName+'/Response'+requestCount+'.txt'),{end:false});
     console.log('--------------------[ /simulation Request '+currentRequestNum+' ]---------------');
 
 });
