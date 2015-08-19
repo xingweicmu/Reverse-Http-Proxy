1734d460b177b99b23914f76641b734a10be5e0c
Xing Wei <xingw1@andrew.cmu.edu>
2015-08-19 10:43:27 -0700

    Added middleware for http requests

---
+++ http-proxy-get-middleware.js
@@ -0,0 +1,163 @@
+exports.getHandler = function getHandler(webRequest, response, next) {
+
+	// Set up dependency
+	var bodyParser = require("body-parser");
+	var fs = require('fs');
+	var http = require('http');
+	var request = require('request');
+	var jar = request.jar();
+
+	// Get parameters from the server
+	var filter = shared_filter;
+	var serviceName = shared_directory;
+	var hostName = shared_hostName;
+	var portNumber = shared_portNumber;
+
+	var headers = webRequest.headers;
+	var currentRequestNum = requestCount;
+	var data = '';
+
+	console.log('--------------------[ simulation Request '+currentRequestNum+ ' ]---------------');
+	console.log('GET Request:'+webRequest.url);
+	console.log('GET Headers:'+JSON.stringify(headers));
+
+	// Create new headers based on webReqeust.headers by removing browser-based info
+	var newHeaders = {};
+	var referer = null;
+	for(var key in webRequest.headers) {
+		var value = webRequest.headers[key];
+		if(key != 'content-length' && key!='host' /*&& key!='referer'*/){
+			newHeaders[key]=value;
+		}
+	}
+	// newHeaders['host']='localhost:9997';
+	// newHeaders['referer']=(referer||'').replace('localhost:9996', '10.33.121.243:9443/vsphere-client');
+	console.log('-> Send Headers:'+JSON.stringify(newHeaders));
+
+	function endsWith(str, suffix) {
+		return str.indexOf(suffix, str.length - suffix.length) !== -1;
+	}
+
+	// Prepare file path by replacing the '/' with '!'
+	var filePath = webRequest.url.replace(new RegExp('/', 'g'), '!');
+	console.log('@@@'+filePath);
+	// Handle text-based content
+	if(!endsWith(webRequest.url, 'png') && !endsWith(webRequest.url, 'jpg') 
+		&& !endsWith(webRequest.url, 'ttf') && !endsWith(webRequest.url, 'woff')){
+		
+		// Prepare redirecting request options
+		var options = {
+			uri:'http://localhost:8080' + webRequest.url, 
+			// headers: newHeaders, 
+			jar:true, 
+		};
+
+  		request(options, function (error, resp, body) {
+    		if (!error) {
+        		console.log(webRequest.url);
+        		// console.log(resp.body);
+
+        		// Add the count by one after receiving the response
+	        	requestCount++;
+
+	        	// rqst - the request sent to the proxy
+				var rqst = {'path':webRequest.path, 'method':'get', 'headers':webRequest.headers};
+
+				// 1. Normalize the request
+				var normalized = {'path':webRequest.path, 'method':'get'};
+				// var cookie = webRequest.headers['cookie'];
+				// var normalized = {'path':webRequest.path, 'method':'get', 'cookie':cookie};
+
+				// 2. Do Hash
+				var hash = require('crypto').createHash('md5').update(JSON.stringify(normalized)).digest("hex");
+				
+				// 3. Create foldername in the format of num-hash-path
+				var foldername = requestCount + '_' + hash + '_' + filePath;
+				// console.log('FILE_NAME: '+foldername);
+				
+				// 4. Create folder
+				fs.mkdir(serviceName+'/'+foldername,function(){
+					
+					// 5. Write to the file
+					fs.writeFile(serviceName+'/'+foldername+'/Request', JSON.stringify(rqst), function(err) {
+						if (err) return console.log('ERR!!!'+err);
+					});
+			        fs.writeFile(serviceName+'/'+foldername+'/Response', body, function(err) {
+						if (err) return console.log('ERR!!!'+err);
+					});
+					fs.writeFile(serviceName+'/'+foldername+'/ResponseHeader', JSON.stringify(resp.headers), function(err) {
+						if (err) return console.log('ERR!!!'+err);
+					});
+
+					// 6. Send back the response from the server
+		        	response.write(resp.body);
+					response.end();
+				});
+			}
+    		else {
+    			console.log("ERROR in sending/receving the request " + webRequest.path);
+    		}
+		});
+	}
+	// Handle with images or font, which requires to be encoded in binary
+	else{	
+		options = {
+			host: hostName
+			, port: portNumber
+			, path: webRequest.url
+			, jar:true
+		}
+
+		var request = http.get(options, function(res){
+			
+			var imagedata = '';
+			res.setEncoding('binary');
+
+			res.on('data', function(chunk){
+			    imagedata += chunk;
+			})
+
+			res.on('end', function(){
+				
+				requestCount++;
+				var rqst = {'path':webRequest.path, 'method':'get', 'headers':webRequest.headers};
+				
+				// 1. Normalize the request
+				var normalized = {'path':webRequest.path, 'method':'get'};
+				// var cookie = webRequest.headers['cookie'];
+				// var normalized = {'path':webRequest.path, 'method':'get', 'cookie':cookie};
+				
+				// 2. Do Hash
+				var hash = require('crypto').createHash('md5').update(JSON.stringify(normalized)).digest("hex");
+				
+				// 3. Create foldername in the format of num-hash-path
+				var foldername = requestCount + '_' + hash + '_' + filePath;
+				
+				// 4. Create folder
+				fs.mkdir(serviceName+'/'+foldername,function(){
+					
+					// 5. Write the request to file 
+					fs.writeFile(serviceName+'/'+foldername+'/Request', JSON.stringify(rqst), function(err) {
+						if (err) return console.log('ERR!!!'+err);
+					});
+
+					fs.writeFile(serviceName+'/'+foldername+'/Response', imagedata, 'binary', function(err){
+	            		if (err) return console.log('ERR!!!'+err);
+	            		console.log('File saved.')
+	        		})
+
+					fs.writeFile(serviceName+'/'+foldername+'/ResponseHeader', JSON.stringify(res.headers), 'binary', function(err){
+						if (err) throw err
+					})
+
+					// 6. Send back the data
+					response.end(imagedata, 'binary');
+				});
+
+			})
+
+  		})
+	}
+	console.log('--------------------[ /simulation Request '+currentRequestNum+' ]---------------');
+
+}
---
+++ http-proxy-post-middleware.js
@@ -0,0 +1,112 @@
+exports.postHandler = function postHandler(webRequest, response, next) {
+
+	// Set up dependencies
+	var bodyParser = require("body-parser");
+	var fs = require('fs');
+	var http = require('http');
+	var HashMap = require('hashmap');
+	var request = require('request');
+	var jar = request.jar();
+
+
+	// Get parameters from the server
+	var filter = shared_filter;
+	var serviceName = shared_directory;
+	var hostName = shared_hostName;
+	var portNumber = shared_portNumber;
+
+	var hashMap = new HashMap();
+	var data = '';
+	var headers = webRequest.headers;
+	var currentRequestNum = requestCount;
+	var queryBody = webRequest.body;
+	
+	console.log('--------------------[ simulation Request '+currentRequestNum+ ' ]---------------');
+	console.log('POST Request:'+webRequest.url);
+	console.log('POST Headers:'+JSON.stringify(headers));
+	console.log('POST Body:'+JSON.stringify(queryBody));
+
+	var data = JSON.stringify(queryBody);
+	var currentCount = requestCount;
+
+	function callback(error, cbresponse, body) {
+    	console.log('--------------------[ endpoint Response '+currentCount+ ' ]---------------');
+		var cbheaders = cbresponse.headers;
+		console.log('Headers from endpoint:'+JSON.stringify(cbheaders));
+		
+		var rtnHeaders = {};
+		for(var key in cbheaders) {
+    		var value = cbheaders[key];
+			if(key!='content-length'&&key!='host' && key != 'location'){
+				rtnHeaders[key]=value;
+			}
+		}
+
+		// IMPORTANT: the 'location' should be the host name running the proxy
+		console.log('http://'+hostName+':'+listenPort+'/');
+		rtnHeaders['location']='http://localhost:'+listenPort+'/';
+
+		requestCount++;
+
+		var filePath = webRequest.url.replace(new RegExp('/', 'g'), '!');
+		var rqst = {'path':webRequest.path, 'method':'post', 'headers':webRequest.headers, 'body':data};
+
+		// 1. Normalize the request
+		var normalized = {'path':webRequest.path, 'method':'post', 'body':data};
+		// 2. Do Hash
+		var hash = require('crypto').createHash('md5').update(JSON.stringify(normalized)).digest("hex");
+		// 3. Create foldername in the format of num-hash-path
+		var foldername = requestCount + '_' + hash + '_' + filePath;
+		console.log(foldername);
+		// 4. Create folder
+		fs.mkdir(serviceName+'/'+foldername,function(){
+			// 5. Write file
+			fs.writeFile(serviceName+'/'+foldername+'/Request', JSON.stringify(rqst), function(err) {
+				if (err) throw err;
+			});
+
+			fs.writeFile(serviceName+'/'+foldername+'/ResponseHeader', JSON.stringify(rtnHeaders), function (err) {
+	  			if (err) throw err;
+			});
+
+			fs.writeFile(serviceName+'/'+foldername+'/Response', body, function (err) {
+	  			if (err) throw err;
+			});
+
+			console.log('Response Code:'+cbresponse.statusCode); // + '   Body:'+body);
+
+			// 6. Send back the response
+			response.writeHead(cbresponse.statusCode,rtnHeaders);
+			response.write(body);
+			response.end();
+		});
+
+        console.log('--------------------[ /endpoint Response '+currentCount+ ' ]---------------');
+	};
+
+	// Prepare new headers based on webRequest.headers
+	var newHeaders = {};
+	for(var key in webRequest.headers) {
+    	var value = webRequest.headers[key];
+		//console.log('HEADER:'+key+':'+value);
+		if(key!='content-length'&&key!='host'){
+			newHeaders[key]=value;
+		}
+	}
+
+	console.log('Send Headers:'+JSON.stringify(newHeaders));
+
+	var options = {
+		uri:'http://localhost:8080'+webRequest.url
+		//, headers: {"content-type":"text/xml; charset=utf-8","soapaction":"urn:vim25/5.5","user-agent":"VMware vim-java 1.0"}
+		// , headers: newHeaders
+		, headers: webRequest.headers
+		, jar:true
+		, body:data
+	};
+
+	// Redirect the POST request
+	request.post(options, callback);
+    console.log('--------------------[ /simulation Request '+currentRequestNum+' ]---------------');
+
+}
